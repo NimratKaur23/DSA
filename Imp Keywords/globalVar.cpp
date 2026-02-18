5bfc796165f3a5375db91bcffd22ec922c8f297b
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-02-18 22:59:15 +0530

    Add files via upload

---
+++ Imp Keywords/globalVar.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+//global variable
+int score=15;
+
+void a(int& i) {
+    cout<<score<<" in a"<<endl;
+    
+    cout<<i<<endl;
+}
+
+void b(int& i) {
+    cout<<score<<" in b"<<endl;
+    cout<<i<<endl;
+
+}
+
+int main() {
+
+     cout<<score<<" in main"<<endl;
+
+    int i=5;
+    a(i);
+    b(i);
+    
+
+   
+
+    return 0;
+}
---
+++ Imp Keywords/macros.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//macro created
+#define PI 3.14
+
+int main() {
+
+    int r=5;
+    double area= PI * r * r;
+    cout<<"AREA IS: "<<area<<endl;
+
+
+
+    return 0;
+}
